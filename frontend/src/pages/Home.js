bb534898d4c5145815fb4bf64f8c9f51dd82ca63
jdtheefirst <jngatia045@gmail.com>
2024-01-04 07:10:48 -0900

    landing page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,13 @@
 import {
   Box,
    Button,
-   Container,
+   Image,
    Text
   } from "@chakra-ui/react";
   import { useNavigate } from "react-router-dom";
   import ErrorBoundary from "../components/ErrorBoundary";
+  import "../App.css"
+
 
   
   function Homepage() {
@@ -19,19 +21,61 @@ import {
   
     return (
       <ErrorBoundary fallback={<p>Something went wrong</p>}>
-       <Box display={"flex"} width={"100%"}>
-       <Box display={"flex"} justifyContent={"flex-end"} className="sideDrawer" width={"100%"}><Button background={"transparent"} textColor={"white"} _hover={{backgroundColor: "transparent", color: "green"}} onClick={ () => navigate("/login")}>Login/Sign Up</Button></Box> 
-       <Box
-          display="flex"
-          justifyContent="space-between"
-          w="100%"
-          h="91.5vh"
-          p="0.2rem"
-        >
-          Mzeva
+      <Box display="flex" flexDir={"column"}  width="100%"> 
+        <Box display="flex" justifyContent="flex-end" className="sideDrawer" backgroundColor={"Background"} width="100%">
+          <Button background="transparent" _hover={{backgroundColor: "transparent", color: "green"}}>About</Button>
+          <Button background="transparent"  _hover={{backgroundColor: "transparent", color: "green"}}>Events</Button>
+          <Button background="transparent" _hover={{backgroundColor: "transparent", color: "green"}} onClick={() => navigate("/login")}>Login/Sign Up</Button>
         </Box>
-       </Box>
-      </ErrorBoundary>
+        <Text textAlign={"center"} fontWeight={"bold"} fontSize={"2xl"} backgroundColor={"Background"}m={1} p={5} borderRadius={3}>Welcome to World-Samma, where discipline meets passion. Begin your path to mastery today.</Text>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems={"center"}
+          w="100%"
+          height={"50%"}
+          p={0}
+          m={0}
+       
+        >
+           <Image
+          src='https://res.cloudinary.com/dvc7i8g1a/image/upload/v1704379830/samma1_sfkycx.jpg'
+          alt=''
+          borderRadius='lg'
+          height={"90%"}
+          _hover={{
+            transform: 'scale(1.05)', // Scales up the image to 105% of its original size
+            transition: 'transform 0.2s', // Smooth transition effect
+          }}
+          m={2}
+        />
+        <Image
+          src='https://res.cloudinary.com/dvc7i8g1a/image/upload/v1704379830/samma_dm0t2d.jpg'
+          alt=''
+          borderRadius='lg'
+          height={"90%"}
+          _hover={{
+            transform: 'scale(1.05)', // Scales up the image to 105% of its original size
+            transition: 'transform 0.2s', // Smooth transition effect
+          }}
+          m={2}
+        />
+        <Image
+          src='https://res.cloudinary.com/dvc7i8g1a/image/upload/v1704379830/samma1_sfkycx.jpg'
+          alt=''
+          borderRadius='lg'
+          height={"90%"}
+          _hover={{
+            transform: 'scale(1.05)', // Scales up the image to 105% of its original size
+            transition: 'transform 0.2s', // Smooth transition effect
+          }}
+          m={2}
+        />
+       
+        </Box>
+        <Box display={"flex"} justifyContent={"center"} alignItems={"center"}><Button borderRadius={20} backgroundColor={"#a432a8"} onClick={() => navigate("/login")}>Get Started</Button></Box>
+      </Box>
+    </ErrorBoundary>
     );
   }
   
---
+++ frontend/src/pages/Logins.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+    Box,
+    Container,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+  } from "@chakra-ui/react";
+  import Login from "../Authentication/Login";
+  import Signup from "../Authentication/SignUp";
+
+const Logins = () => {
+  return (
+    <Container maxW="xl" centerContent>
+         <Box
+            display="flex"
+            justifyContent="center"
+            p={3}
+            bg="white"
+            w="100%"
+            m="40px 0 15px 0"
+            borderRadius="lg"
+            borderWidth="1px"
+          >
+  
+            <Text fontWeight={"bold"} fontSize="3xl">Welcome to World Samma</Text>
+          </Box>
+          <Box bg="black" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+            <Tabs isFitted variant="soft-rounded">
+              <TabList mb="1em">
+                <Tab>Login</Tab>
+                <Tab>Sign Up</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Login />
+                </TabPanel>
+                <TabPanel>
+                  <Signup />
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </Box>
+    </Container>
+  )
+}
+
+export default Logins
