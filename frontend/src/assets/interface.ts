93aba624c8ddeadfa7a156f0e93ef2ae31218497
jdtheefirst <jngatia045@gmail.com>
2024-02-15 10:30:40 -0900

    states

--- frontend/src/assets/interface.ts
+++ frontend/src/assets/interface.ts
@@ -8,6 +8,25 @@ export interface IState {
 	getStateByCodeAndCountry?(): IState;
 	getStateByCode?(): IState;
 }
+export interface Timezones {
+	zoneName: string;
+	gmtOffset: number;
+	gmtOffsetName: string;
+	abbreviation: string;
+	tzName: string;
+}
+export interface ICountry {
+	name: string;
+	phonecode: string;
+	isoCode: string;
+	flag: string;
+	currency: string;
+	latitude: string;
+	longitude: string;
+	timezones?: Timezones[];
+	getAllCountries?(): ICountry[];
+	getCountryByCode?(): ICountry;
+}
 
 export const compare = (a: any, b: any) => {
 	if (a.name < b.name) return -1;
