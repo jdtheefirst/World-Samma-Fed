298595237c5e68979c9249c0ca09c3b8241c5a31
jdtheefirst <jngatia045@gmail.com>
2024-07-02 21:26:55 +0300

    rrrr

--- backend/index.js
+++ backend/index.js
@@ -95,15 +95,18 @@ app.use("/api/translate", useTranslator);
 app.use("/api/donate", donateRouter);
 app.use("/api/poll", voteRouter);
 
-const __dirname1 = path.resolve(__dirname, "backend"); // Adjust based on your server script location
+const __dirname1 = path.resolve(__dirname, "backend");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+  // Serve static files from the frontend build directory
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
+  // Serve index.html for all other routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname1, "../frontend/build", "index.html"));
   });
 } else {
+  // Development mode API response
   app.get("/", (req, res) => {
     res.send("API is running..");
   });
